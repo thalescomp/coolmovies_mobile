e77b16ee23e20f6c924c244fe93cb2572d6cb594
Thales Carvalho <thales@ituring.com.br>
2023-10-10 01:18:56 -0300

    implement providers to manage movie global state and login info with secure storage

---
+++ lib/providers/user_auth_provider.dart
@@ -0,0 +1,46 @@
+import 'package:coolmovies_mobile/models/user.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class UserAuthNotifier extends StateNotifier<Map<String, dynamic>> {
+  UserAuthNotifier() : super({'isLogged': false, 'isLoginSkipped': false});
+
+  Future<void> uptadeStorage() async {
+    const storage = FlutterSecureStorage();
+    await storage.write(key: 'isLogged', value: state['isLogged'].toString());
+    await storage.write(key: 'name', value: state['name']);
+    await storage.write(key: 'id', value: state['id']);
+    await storage.write(
+        key: 'isLoginSkipped', value: state['isLoginSkipped'].toString());
+  }
+
+  Future<void> deleteStorage() async {
+    const storage = FlutterSecureStorage();
+    await storage.deleteAll();
+  }
+
+  void loginUser(User user) async {
+    state = {
+      ...state,
+      'id': user.id,
+      'name': user.name,
+      'isLogged': true,
+      'isLoginSkipped': false
+    };
+    await uptadeStorage();
+  }
+
+  void logoutUser() async {
+    state = {'isLogged': false, 'isLoginSkipped': false};
+    await deleteStorage();
+  }
+
+  void toggleSkipLogin() async {
+    state = {...state, 'isLoginSkipped': !state['isLoginSkipped']};
+    await uptadeStorage();
+  }
+}
+
+final userAuthProvider =
+    StateNotifierProvider<UserAuthNotifier, Map<String, dynamic>>(
+        (ref) => UserAuthNotifier());
